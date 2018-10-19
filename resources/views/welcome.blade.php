e981d9eacce9b2d543d3b96c913f1270c271d211
SlowV <31509114+SlowV@users.noreply.github.com>
2018-10-19 22:33:19 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,159 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="{{asset("img/favicon.png")}}">
+	<title>20 - 10 for you!</title>
 
-        <title>Laravel</title>
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:200" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:100" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+	<!-- Styles -->
+	<style>
+		* {
+			padding: 0;
+			margin: 0;
+		}
 
-            .full-height {
-                height: 100vh;
-            }
+		html, body {
+			background-color: #fff;
+			color: #636b6f;
+			font-family: 'Raleway', sans-serif;
+			height: 100vh;
+			margin: 0;
+			background: url('{{asset('img/bg-content.jpg')}}') no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+			position: relative;
+			overflow: hidden;
+		}
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+		.full-height {
+			height: 100vh;
+		}
 
-            .position-ref {
-                position: relative;
-            }
+		.flex-center {
+			align-items: center;
+			display: flex;
+			justify-content: center;
+		}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+		.position-ref {
+			position: relative;
+		}
 
-            .content {
-                text-align: center;
-            }
+		.top-right {
+			position: absolute;
+			right: 10px;
+			top: 18px;
+		}
 
-            .title {
-                font-size: 84px;
-            }
+		.content {
+			text-align: right;
+			position: fixed;
+		}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+		.title {
+			font-size: 84px;
+			z-index: auto;
+			color: #000;
+		}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+		.links > a {
+			color: #636b6f;
+			padding: 0 25px;
+			font-size: 12px;
+			font-weight: 600;
+			letter-spacing: .1rem;
+			text-decoration: none;
+			text-transform: uppercase;
+		}
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+		.m-b-md {
+			margin: 0 0 30px 550px;
+		}
+	</style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+	@if (Route::has('login'))
+		<div class="top-right links">
+			@auth
+				<a href="{{ url('/admin/product') }}">Trang chủ</a>
+			@else
+				<a href="{{ route('login') }}">Đăng nhập</a>
+				<a href="{{ route('register') }}">Đăng ký</a>
+			@endauth
+		</div>
+	@endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+	<div class="content">
+		<div class="title m-b-md">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+			Welcome
+
+			Hana Store
+			@endauth
+		</div>
+	</div>
+
+</div>
+</body>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script>
+    function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    $(document).ready(function () {
+        setInterval(function () {
+            var screenHeight = $(document).height();
+            var screenWidth = $(document).width();
+
+            var startLeft = getRandomArbitrary(0, screenWidth / 1, 5);
+
+            var timeRun = getRandomArbitrary(4000, 6000);
+            var opacityR = Math.random() * (0.9 - 0.2) + 0.2;
+            var sizeR = getRandomArbitrary(5, 20);
+
+            var endLeft = getRandomArbitrary(startLeft - 100, startLeft + 100);
+
+            var snow = document.createElement('span');
+
+            $(snow).addClass('snow-item fas fa-heart').css({
+                'position': 'absolute',
+                'z-index': 'auto',
+                'color': '#DC706A',
+                'display': 'block',
+                'top': 0,
+                'height': '10px',
+                'left': startLeft,
+                'opacity': opacityR,
+                'font-size': sizeR + 'px'
+            })
+                .appendTo('body')
+                .animate({
+                    'top': screenHeight - sizeR,
+                    'left': endLeft
+                }, {
+                    duration: timeRun,
+                    easing: 'linear',
+                    complete: function () {
+                        $(this).fadeOut('fast', function () {
+                            $(this).remove();
+                        });
+                    }
+                });
+
+        }, 500);
+    });
+</script>
 </html>
