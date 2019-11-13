9a0ae8212393ce4788957cfae655dd6b34c26b38
SlowV <quocviet.hn98@gmail.com>
2019-11-13 17:24:23 +0700

    My love!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" href="img/favicon1.png">
     <meta property="og:image" content="https://demo-app-html.herokuapp.com/projectSem1/public/img/MyMusic/Logo.png"/>
     <meta property="og:title" content="13 - 11 for you!"/>
-    <meta property="og:description" content='Can I say "I love you" for you?'/>
+    <meta property="og:description" content='Can I say "I love you"?'/>
     <meta name="author" content="SlowV">
     <title>13 - 11 for you!</title>
 
